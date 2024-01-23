2d238c60b8671b108a24af13f04fe7c05b825d17
e-brent <enb40@pitt.edu>
2024-01-23 09:39:11 -0500

    Started creating routes for different app screens

--- MercuryMobileApp/App.js
+++ MercuryMobileApp/App.js
@@ -1,13 +1,58 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list'; //might be using?? maybe not, still figuring out best way to do forms
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home"  
+          component={HomeScreen} 
+        />
+        <Stack.Screen 
+          name="DOFScreen"  
+          component={DOFScreen} 
+        /> 
+
+        {/* can add more pages here as they're made */}
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+// Home screen of app
+const HomeScreen = ({navigation}) => {
+  return(
+    <View style={styles.container}>
+      <Text>Welcome to Mercury Works!</Text>
+      <Button 
+        title= "Go to DOF calculator"
+        onPress={() => navigation.navigate("DOFScreen")}
+      />
+    </View>
+  )
+}
+
+// DOF screen of app
+const DOFScreen = ({route, navigation}) => {
+  return(
+    <View style={styles.container}>
+      <Text>DOF Calculator</Text>
+      <Button 
+        title= "Back to Home"
+        onPress={() => navigation.navigate("HomeScreen")}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +63,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App;
