42538b7160be3372919ca1165ef9e744eea4fd66
e-brent <enb40@pitt.edu>
2024-02-26 17:51:29 -0500

    reciprocity calculator is functioning, possibly still some stylistic changes to come. created file to move the DOF calculator screen to

---
+++ MercuryMobileApp/screens/dofCalculator.js
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { StyleSheet, View , Text, SafeAreaView, Button } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list'; 
+import SegmentedControlTab from "react-native-segmented-control-tab";
